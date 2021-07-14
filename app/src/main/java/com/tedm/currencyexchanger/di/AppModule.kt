8bbeb4a5726aec657488682e6496d0306de4ba07
Tadas <tadasgailevicius@yahoo.com>
2021-07-14 20:31:56 +0300

    Update AppModule.kt DI

--- app/src/main/java/com/tedm/currencyexchanger/di/AppModule.kt
+++ app/src/main/java/com/tedm/currencyexchanger/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.tedm.currencyexchanger.di
 
+import android.content.Context
+import androidx.room.Room
+import com.tedm.currencyexchanger.data.local.BalanceDatabase
 import com.tedm.currencyexchanger.data.remote.CurrencyApi
 import com.tedm.currencyexchanger.other.Constants.Companion.BASE_URL
+import com.tedm.currencyexchanger.other.Constants.Companion.DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,6 +18,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideBalanceDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, BalanceDatabase::class.java , DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideBalanceDao(db: BalanceDatabase) = db.balanceDao()
 
     @Singleton
     @Provides
