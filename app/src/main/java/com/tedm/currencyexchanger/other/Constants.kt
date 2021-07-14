3a4b0b51b2110bc2cec51e4e069a4de3e2f5fb59
Tadas <tadasgailevicius@yahoo.com>
2021-07-14 19:53:31 +0300

    Add Constants.kt Constants

---
+++ app/src/main/java/com/tedm/currencyexchanger/other/Constants.kt
@@ -0,0 +1,8 @@
+package com.tedm.currencyexchanger.other
+
+class Constants {
+    companion object {
+        const val DATABASE_NAME = "currency_db"
+        const val BASE_URL = "https://api.exchangeratesapi.io"
+    }
+}
