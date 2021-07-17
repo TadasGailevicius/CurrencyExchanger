2000d0d9581605135af1ae1a4f44226ad810e9ee
Tadas <tadasgailevicius@yahoo.com>
2021-07-17 21:31:27 +0300

    Update Constants.kt Constants

--- app/src/main/java/com/tedm/currencyexchanger/other/Constants.kt
+++ app/src/main/java/com/tedm/currencyexchanger/other/Constants.kt
@@ -2,7 +2,7 @@ package com.tedm.currencyexchanger.other
 
 class Constants {
     companion object {
-        const val DATABASE_NAME = "currency_db"
+        const val DATABASE_NAME = "balance_db"
         const val BASE_URL = "https://api.exchangeratesapi.io"
     }
 }
